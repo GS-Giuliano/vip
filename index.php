c51fe027f26201c6352c915ccabc367a795b6253
Giuliano Nascimento <giuliano@Giulianos-iMac.local>
2023-04-09 14:47:02 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" >
+		<title>Vale do Ipitanga</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="img/favicon.png" type="image/x-icon">
+		<!-- Google Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">		
+		<!-- Bootstrap 4.3.1 CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- Slick 1.8.1 jQuery plugin CSS (Sliders) -->
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+		<!-- Fancybox 3 jQuery plugin CSS (Open images and video in popup) -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+		<!-- AOS 2.3.4 jQuery plugin CSS (Animations) -->
+		<link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
+		<!-- FontAwesome CSS -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<!-- Startup 3 CSS (Styles for all blocks) -->
+		<link href="css/style.css" rel="stylesheet" />
+		<!-- jQuery 3.3.1 -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	</head> 
+	<body>
+<!-- Navigation Mobile type 1 -->
+
+<a href="#" class="open_menu bg-light radius_full"><i class="fas fa-bars lh-40"></i></a>
+<div class="navigation_mobile bg-dark type1">
+	<a href="#" class="close_menu color-white"><i class="fas fa-times"></i></a>
+	<div class="px-40 pt-60 pb-60 inner">
+		<div class="logo color-white mb-30">Vale do Ipitanga</div>
+		<div><a href="#participar" class="f-heading f-22 link color-white mb-20">Participar</a></div>	
+		<div><a href="#sabermais" class="f-heading f-22 link color-white mb-20">Saber mais</a></div>	
+		<div><a href="https://padlet.com/valedoipitanga/eventos-nnkxsrg4ha87iy9d" class="link color-white op-3 mb-15">Agenda</a></div>
+		<div><a href="https://padlet.com/valedoipitanga/linha-do-tempo-1zqtq7w0s236te62" class="link color-white op-3 mb-15">História</a></div>
+		<div><a href="https://padlet.com/valedoipitanga/mapa-do-ecossistema-vale-do-ipitanga-8ttzta4f9hliioi6" class="link color-white op-3 mb-15">Mapa</a></div>
+		<!-- <div><a href="https://www.instagram.com/valedoipitanga/" class="link color-white op-3 mb-15">Instagram</a></div>
+		<div><a href="https://www.youtube.com/channel/UCkqSYdBYfkx_KeEApJu65SQ" class="link color-white op-3 mb-15">Youtube</a></div> -->
+						
+		<div class="socials mt-40">
+			<a href="https://www.instagram.com/valedoipitanga/" target="_blank" class="link color-white f-18 mr-20"><i class="fab fa-instagram"></i></a>
+			<a href="https://www.youtube.com/channel/UCkqSYdBYfkx_KeEApJu65SQ" target="_blank" class="link color-white f-18 mr-20"><i class="fab fa-youtube"></i></a>
+		</div>
+		
+	</div>
+</div>		<!-- Header 7 -->
+		<header class="bg-light header_7">
+			<nav class="header_menu_7 pt-30 pb-30 mt-40">
+				<div class="container px-xl-0">
+					<div class="mw-570">
+						<div class="row align-items-center">
+							<div class="col-lg-6">
+								<div class="logo"><img class="img-fluid float-start" src="img/logo.png" alt="Vale do Ipitanga"></div>
+							</div>
+							<div class="col-lg-6 text-right">
+								<a href="#" class="link color-heading medium ml-35">Entrar »</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+			<div class="container px-xl-0 relative text-center text-md-left">
+				<img src="i/header_7_bg.jpg" srcset="i/header_7_bg@2x.jpg 2x" alt="" class="d-none d-md-block ml-700 h-full absolute header_7_bg" />
+				<div class="mw-570 pt-230 pb-165 inner">
+					<div class="mb-3 logo d-block d-xl-none logo_mobile">Vale do Ipitanga</div>
+					<h1>Que bom ter você por aqui!</h1>
+					<div class="mt-20 mw-500 color-heading f-18 medium op-7 text-adaptive"><div>
+					Nosso propósito é conectar, incentivar e catalizar a inovação e empreendedorismo na cidade de Lauro de Freitas na Bahia, em busca de uma sociedade mais vibrante, saudável, equilibrada e justa!
+					</div>
+					</div>
+					<div class="mt-70 d-block d-sm-flex flex-wrap justify-content-center justify-content-md-start buttons">
+						<a href="#participar" class="btn lg action-1">Participar</a>
+						<a href="#sabermais" class="btn ml-10 lg border-gray color-main">Saber mais</a>
+					</div>
+					<div class="mt-20 color-heading text-adaptive">Ao cadastrar você concorda com nossos <a href="termos.html" class="link color-heading">Termos de Uso</a>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<!-- Content 15 -->
+		<section id="sabermais" class="pt-580 pb-160 bg-dark color-white color-filter-dark-3 content_15">
+			<style>
+				.content_15{background-image:url(img/decolalauro.jpg);}
+				@media(-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi){
+					.content_15{background-image:url(img/decolalauro.jpg);}
+				}
+			</style>
+			<div class="container px-xl-0">
+				<div class="row">
+					<div class="col-lg-1">
+					</div>
+					<div class="col-lg-7 col-md-10">
+						<h2 style="background-color: #e83e8c">Vamos fazer diferente?</h2>
+						<div class="mt-15 f-22 text-adaptive">Juntar as Instituições de Ensino, Empresas, Governo e Sociedade Civil Organizada é o desafio do nosso Ecossistema, mas com a dedicação e o <i>give first</i> como premissa, podemos realizar o possível, o desejável e até mesmo o impossível!<br>Esse é o <b>Ecossistema de Inovação e Empreendedorismo do Vale do Ipitanga</b>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Feature 42 -->
+		<section class="bg-light feature_42">
+			<div class="container px-xl-0">
+				<div class="row text-center text-sm-left">
+					<div class="col-xl-5 col-lg-4 col-md-3 col-sm-1 d-none d-sm-block">
+						<img src="i/feature_42_bg.jpg" srcset="i/feature_42_bg@2x.jpg 2x" alt="" class="absolute h-full bg" />
+					</div>
+					<div class="col-sm-1">
+					</div>
+					<div class="col-xl-6 col-lg-7 col-md-8 col-sm-10 pt-105 pb-80 inner">
+						<h2 style="color: #e83e8c" class="mb-25 small">Ligar os pontos</h2>
+						<div class="f-22 color-heading text-adaptive">
+							Tem uma idéia?<br> Tem conhecimento pra compartilhar?<br> Tem dinheiro pra investir?<br> Sabe como pode ajudar?<br><br>Fazendo as conexões certas, dá pra fazer acontecer!
+						</div>
+						<div class="mt-55 mb-70 hr h-2 bg-gray">
+						</div>
+						<div class="row">
+							<div class="mt-40 mt-sm-0 col-xl-5 col-sm-6 block">
+								<div class="mb-20 f-14 text-uppercase semibold sp-20 title">Como posso crescer?</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou um voluntário</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou um idealista</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou um estudante</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou uma Startup</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Quero montar um negócio</a>
+								</div>
+							</div>
+							<div class="mt-40 mt-sm-0 col-xl-5 col-sm-6 block">
+								<div class="mb-20 f-14 text-uppercase semibold sp-20 title">Como posso ajudar?</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou investidor</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou aceleradora</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou hub de inovação</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou consultor/mentor</a>
+								</div>
+								<div>
+									<a href="#" class="link mb-15 color-heading">Sou governo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Content 31 -->
+		<section class="pt-100 pb-70 bg-dark text-center color-white content_31">
+			<div class="container px-xl-0">
+				<div class="row justify-content-center">
+					<h2 class="small col-lg-8">De braços dados</h2>
+					<div class="col-lg-8 mt-25 mb-55 f-18 medium op-7 text-adaptive">					<div>
+Entidades e empresas que tem nos apoiando nesta jornada desafiadora...
+					</div>
+					</div>
+				</div>
+				<div class="row justify-content-center">
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="img/sebrae.png" srcset="img/sebrae.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="img/fecomercio.png" srcset="img/fecomercio.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="img/prefeitura.png" srcset="img/prefeitura.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="img/bnb.png" srcset="img/bnb.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<!-- <div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="img/google.png" srcset="img/google.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="i/content_31_logo_6.png" srcset="i/content_31_logo_6@2x.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="i/content_31_logo_7.png" srcset="i/content_31_logo_7@2x.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-5 col-sm-6">
+						<div class="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
+							<img src="i/content_31_logo_8.png" srcset="i/content_31_logo_8@2x.png 2x" alt="" class="img-fluid" />
+						</div>
+					</div> -->
+				</div>
+			</div>
+		</section>
+
+		<!-- Content 23 -->
+		<section class="pt-100 pb-70 bg-light content_23">
+			<div class="container px-xl-0">
+				<div class="row text-center">
+					<div class="col-sm-4">
+						<img src="i/content_23_img_1.jpg" srcset="i/content_23_img_1@2x.jpg 2x" alt="" class="img-fluid radius10 mb-30" />
+						<img src="img/master.jpg" srcset="img/master.jpg" alt="" class="img-fluid radius10 mb-30" />
+					</div>
+					<div class="col-sm-4">
+						<img src="img/sw.jpg" srcset="img/sw.jpg" alt="" class="img-fluid radius10 mb-30" />
+						<img src="i/content_23_img_4.jpg" srcset="i/content_23_img_4@2x.jpg 2x" alt="" class="img-fluid radius10 mb-30" />
+					</div>
+					<div class="col-sm-4">
+						<img src="i/content_23_img_5.jpg" srcset="i/content_23_img_5@2x.jpg 2x" alt="" class="img-fluid radius10 mb-30" />
+						<img src="i/content_23_img_6.jpg" srcset="i/content_23_img_6@2x.jpg 2x" alt="" class="img-fluid radius10" />
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Content 7 -->
+		<!-- <section class="pt-90 pb-85 bg-dark content_7">
+			<div class="container px-xl-0">
+				<div class="row justify-content-center text-center color-white">
+					<div class="col-lg-8 col-md-10">
+						<div class="f-14 semibold text-uppercase sp-20">Top Business Company of 2020</div>
+						<div class="mt-15 f-22 text-adaptive">If your primary purpose is e-commerce, particularly for products, you should select software which is specifically designed for e-commerce. However, if e-commerce is only a small part of your website’s purpose.
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> -->
+
+		<!-- Call to action 28 -->
+		<section class="pt-105 pb-100 bg-light call_to_action_28">
+			<div class="container px-xl-0">
+				<div class="row justify-content-center">
+					<div class="col-xl-10 d-lg-flex flex-wrap align-items-center justify-content-between">
+						<div class="d-md-flex flex-wrap align-items-center">
+							<img src="i/call_to_action_28_img.png" srcset="img/logo-completo.png 2x" alt="" class="mb-20 mb-md-0 img radius20" />
+							<div id="participar" class="pl-25 inner">
+								<h2 class="small">Participe fazendo seu cadastro</h2>
+								<div class="f-22 color-heading text-adaptive">
+									Queremos conhecer você e saber como poderá ajudar ou ser ajudado!
+								</div>
+							</div>
+						</div>
+						<div>
+							<a href="https://docs.google.com/forms/d/e/1FAIpQLSfJ9q_YExRHlYiIXP0GLrXpYZaB6IYwoFvWPTi3Ft-VW9r9PA/viewform" class="btn mt-30 mt-lg-0 lg border-gray color-main px-4">Cadastro</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Footer 12 -->
+		<footer class="pt-80 pb-50 bg-dark color-white text-center footer_12">
+			<div class="container px-xl-0">
+				<div class="lh-40 f-18 medium">
+					<a href="https://padlet.com/valedoipitanga/eventos-nnkxsrg4ha87iy9d" class="link color-white mx-20">Agenda</a>
+					<a href="https://padlet.com/valedoipitanga/linha-do-tempo-1zqtq7w0s236te62" class="link color-white mx-20">História</a>
+					<a href="https://padlet.com/valedoipitanga/mapa-do-ecossistema-vale-do-ipitanga-8ttzta4f9hliioi6" class="link color-white mx-20">Mapa do ecossistema</a>					
+					<a href="https://www.instagram.com/valedoipitanga/" class="link color-white mx-20">Instagram</a>
+					<a href="https://www.youtube.com/channel/UCkqSYdBYfkx_KeEApJu65SQ" class="link color-white mx-20">Youtube</a>
+				</div>
+				<div class="mt-60 text-adaptive"> </div>
+			</div>
+		</footer>
+
+		<!-- forms alerts -->
+		<div class="alert alert-success alert-dismissible fixed-top alert-form-success" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			Thanks for your message!
+		</div>
+		<div class="alert alert-warning alert-dismissible fixed-top alert-form-check-fields" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			Please, fill in required fields.
+		</div>
+		<div class="alert alert-danger alert-dismissible fixed-top alert-form-error" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<div class="message">An error occurred while sending data :( Please, check if your hosting supports PHP and check how to set form data sending <a href="https://designmodo.com/startup/documentation/#form-handler" target="_blank" class="link color-transparent-white">here</a>.</div>
+		</div>
+
+		<!-- gReCaptcha popup (uncomment if you need a recaptcha integration) -->
+		<!--
+		<div class="bg-dark op-8 grecaptcha-overlay"></div>
+		<div class="bg-light radius10 w-350 h-120 px-20 pt-20 pb-20 grecaptcha-popup">
+			<div class="w-full h-full d-flex justify-content-center align-items-center">
+				<div id="g-recaptcha" data-sitekey="PUT_YOUR_SITE_KEY_HERE"></div>
+			</div>
+		</div>
+		<script src="https://www.google.com/recaptcha/api.js?render=explicit" async defer></script>
+		-->
+		<!-- Bootstrap 4.3.1 JS -->
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+		<!-- Fancybox 3 jQuery plugin JS (Open images and video in popup) -->
+		<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+		<!-- Google maps JS API
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDP6Ex5S03nvKZJZSvGXsEAi3X_tFkua4U"></script> -->
+		<!-- Slick 1.8.1 jQuery plugin JS (Sliders) -->
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+		<!-- AOS 2.3.4 jQuery plugin JS (Animations) -->
+		<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+		<!-- Maskedinput jQuery plugin JS (Masks for input fields) -->
+		<script src="js/jquery.maskedinput.min.js"></script>
+		<!-- Startup 3 JS (Custom js for all blocks) -->
+		<script src="js/script.js"></script>
+	</body>
+</html>
